63574f5ad7a6f75c04c05e0d33d07edce3ba5790
Carlos Lobo <86011416+CarlosLoboxyz@users.noreply.github.com>
2021-12-29 22:46:30 -0400

    Remove gruvbox color from fzf

--- .config/nixpkgs/console.nix
+++ .config/nixpkgs/console.nix
@@ -91,7 +91,6 @@
 			defaultCommand = "fd -H --type f --color=never";
 			defaultOptions = [
 				"--border" "--info=inline" "--height=100" "--layout=default" 
-				"--color=bg+:#3c3836,bg:#32302f,spinner:#fb4934,hl:#928374,fg:#ebdbb2,header:#928374,info:#8ec07c,pointer:#fb4934,marker:#fb4934,fg+:#ebdbb2,prompt:#fb4934,hl+:#fb4934"
 			];
 			#changeDirWidgetCommand = "";
 			#changeDirWidgetOptions = [  ];
